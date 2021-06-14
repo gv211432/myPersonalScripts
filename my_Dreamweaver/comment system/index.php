577253cfbcb48af1f3bfcabac6be476b5b8b5252
Gaurav Vishwakarma <75873027+gv211432@users.noreply.github.com>
2021-06-14 22:34:53 +0530

    Add files via upload

---
+++ my_Dreamweaver/comment system/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Comment Box</title>
+<link rel="stylesheet" type="text/css" href="">
+<script type="text/javascript" src="uncompressed-jquery.js"></script>
+<script type="text/javascript" src="script.js"></script>
+</head>
+<body>
+
+	<div class="wrapper">
+		<div class="page-data">Page data is in here</div>
+		<div class="comment-wrapper"><h3>User feedback...</h3>
+		.
+
+		</div>
+
+
+	</div>
+
+</body>xa
+</html>
